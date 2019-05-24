0d4ca9bab44bf8324d3839e4f1e502efa7bf4c06
fggo <jnuho@outlook.com>
2019-05-24 15:19:35 +0900

    .

--- food/src/com/kh/food/gui/InitPageFrame.java
+++ food/src/com/kh/food/gui/InitPageFrame.java
@@ -3,18 +3,10 @@ package com.kh.food.gui;
 import javax.swing.*;
 
 public class InitPageFrame extends JFrame{
-//		System.out.println("=== 홈페이지 ===");
-//		System.out.println("1. 회원 가입");
-//		System.out.println("2. 로그인");
-//		System.out.println("3. 로그오프");
-//		System.out.println("4. 주문 하기");
-//		System.out.println("5. 주문 조회");
-//		System.out.println("6. 손님현황 조회");
-//		System.out.println("0. 프로그램 종료.");
 
 	JButton signUpBtn = new JButton("회원가입");
 	JButton signInBtn = new JButton("로그인");
-	JButton logoffBtn = new JButton("로그아웃");
+	JButton logOffBtn = new JButton("로그아웃");
 	JButton orderBtn = new JButton("주문하기");
 	JButton orderViewBtn = new JButton("주문조회");
 	JButton menuBtn = new JButton("메뉴");
@@ -22,4 +14,16 @@ public class InitPageFrame extends JFrame{
 	JButton adminPageBtn = new JButton("관리자");
 
 	JTextField phone;
+
+	public InitPageFrame() {
+		// TODO Auto-generated constructor stub
+		add(signUpBtn);
+		add(signInBtn);
+		add(logOffBtn);
+	
+		
+		setVisible(true);
+	}
+	
+
 }
